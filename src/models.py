ef822c0e190ab38070bc31cddda57e7647a250b3
sanushshakya <shakyasanush7@gmail.com>
2026-06-04 20:43:53 +0100

    feat: add GoogleOAuth2Provider model

--- src/models.py
+++ src/models.py
@@ -1,47 +1,19 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from src.database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class Organisation(Base):
+Base = declarative_base()
+
+class GoogleOAuth2Provider(Base):
     """
-    Represents an organisation in the system.
-
-    Attributes:
-        id (int): Unique identifier for the organisation.
-        name (str): Name of the organisation.
-        description (str): Description of the organisation.
-        users (list[User]): List of users belonging to this organisation.
+    SQLAlchemy model for storing Google OAuth2 provider details.
+    This model is used to store the client ID and client secret required for authenticating with Google's OAuth2 service.
     """
 
-    __tablename__ = "organisations"
+    __tablename__ = "google_oauth2_providers"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    description = Column(String)
+    client_id = Column(String, unique=True, index=True, nullable=False)
+    client_secret = Column(String, nullable=False)
 
-    # Relationship to associated users
-    users = relationship("User", back_populates="organisation")
-
-class User(Base):
-    """
-    Represents a user in the system.
-
-    Attributes:
-        id (int): Unique identifier for the user.
-        name (str): Name of the user.
-        email (str): Email address of the user.
-        role (str): Role of the user (e.g., 'admin', 'user').
-        organisation_id (int): ID of the organisation this user belongs to.
-        organisation (Organisation): The organisation this user belongs to.
-    """
-
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    role = Column(String)
-    organisation_id = Column(Integer, ForeignKey("organisations.id"), nullable=False)
-
-    # Relationship to the associated organisation
-    organisation = relationship("Organisation", back_populates="users")
+    def __repr__(self):
+        return f"<GoogleOAuth2Provider(id={self.id}, client_id={self.client_id})>"
